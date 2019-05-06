b54d4f3dfc3f8de5b564072c054a9ce4d719713b
Cerephenien <dtadaras@gmail.com>
2019-05-06 15:51:19 +0300

    Anglicalisation and structure rewamp

--- js/function.js
+++ js/function.js
@@ -1,51 +1,51 @@
 "use strict";
-function menu_mygtukai_kontaktai() {
+function menu_button_contacts() {
     sort.classList.remove("sort_active");
     sort.classList.add("sort_hidden"); 
     hero.classList.add("hero_hidden");
     hero.classList.remove("hero_active");
-    hero_mygtukas.classList.remove("hero_mygtukas_active");
-    hero_mygtukas.classList.add("hero_mygtukas_hidden");
+    hero_button.classList.remove("hero_button_active");
+    hero_button.classList.add("hero_button_hidden");
     content.classList.add("content_large");
     content.classList.remove("content_mid");
     sort_container.classList.remove("sort_container_active");
     sort_container.classList.add("sort_container_hidden");
-    naujienos.classList.add("naujienos_hidden");
+    news.classList.add("news_hidden");
     card.classList.add("card_hidden");
-    apie_mus.classList.add("apie_mus_hidden");
-    kontaktai.classList.remove("kontaktai_hidden");
+    about.classList.add("about_hidden");
+    contact.classList.remove("contact_hidden");
 }
-function menu_mygtukai_apie_mus() {
+function menu_button_about() {
     sort.classList.remove("sort_active");
     sort.classList.add("sort_hidden");  
     hero.classList.add("hero_hidden");
     hero.classList.remove("hero_active"); 
-    hero_mygtukas.classList.remove("hero_mygtukas_active");
-    hero_mygtukas.classList.add("hero_mygtukas_hidden");
+    hero_button.classList.remove("hero_button_active");
+    hero_button.classList.add("hero_button_hidden");
     content.classList.remove("content_mid");
     content.classList.add("content_large");
     sort_container.classList.remove("sort_container_active");
     sort_container.classList.add("sort_container_hidden");
-    naujienos.classList.add("naujienos_hidden");
+    news.classList.add("news_hidden");
     card.classList.add("card_hidden");
-    apie_mus.classList.remove("apie_mus_hidden");
-    kontaktai.classList.add("kontaktai_hidden");
+    about.classList.remove("about_hidden");
+    contact.classList.add("contact_hidden");
 }
-function menu_mygtukai_naujienos() {
+function menu_button_news() {
     sort.classList.remove("sort_active");
     sort.classList.add("sort_hidden"); 
     hero.classList.add("hero_hidden");
     hero.classList.remove("hero_active");
-    hero_mygtukas.classList.remove("hero_mygtukas_active");
-    hero_mygtukas.classList.add("hero_mygtukas_hidden");
+    hero_button.classList.remove("hero_button_active");
+    hero_button.classList.add("hero_button_hidden");
     content.classList.remove("content_mid");
     content.classList.add("content_large");
     sort_container.classList.remove("sort_container_active");
     sort_container.classList.add("sort_container_hidden");
-    naujienos.classList.remove("naujienos_hidden");
+    news.classList.remove("news_hidden");
     card.classList.add("card_hidden");
-    apie_mus.classList.add("apie_mus_hidden");
-    kontaktai.classList.add("kontaktai_hidden");
+    about.classList.add("about_hidden");
+    contact.classList.add("contact_hidden");
 }
 function sort_active() {
    console.log("hi");
@@ -54,30 +54,30 @@ function sort_active() {
     sort.classList.remove("sort_hidden");
     hero.classList.remove("hero_active");
     hero.classList.add("hero_hidden");
-    hero_mygtukas.classList.remove("hero_mygtukas_active");
-    hero_mygtukas.classList.add("hero_mygtukas_hidden");
+    hero_button.classList.remove("hero_button_active");
+    hero_button.classList.add("hero_button_hidden");
     content.classList.add("content_mid");
     content.classList.remove("content_large");
     sort_container.classList.add("sort_container_active");
     sort_container.classList.remove("sort_container_hidden"); 
-    naujienos.classList.add("naujienos_hidden");
+    news.classList.add("news_hidden");
     card.classList.remove("card_hidden");
-    apie_mus.classList.add("apie_mus_hidden");
-    kontaktai.classList.add("kontaktai_hidden");
+    about.classList.add("about_hidden");
+    contact.classList.add("contact_hidden");
 }
 function homepage() {
     sort.classList.remove("sort_active");
     sort.classList.add("sort_hidden"); 
     hero.classList.remove("hero_hidden");
     hero.classList.add("hero_active");
-    hero_mygtukas.classList.add("hero_mygtukas_active")
-    hero_mygtukas.classList.remove("hero_mygtukas_hidden");
+    hero_button.classList.add("hero_button_active")
+    hero_button.classList.remove("hero_button_hidden");
     content.classList.remove("content_mid");
     content.classList.add("content_large");
     sort_container.classList.remove("sort_container_active");
     sort_container.classList.add("sort_container_hidden");
-    naujienos.classList.remove("naujienos_hidden");
+    news.classList.remove("news_hidden");
     card.classList.add("card_hidden");
-    apie_mus.classList.add("apie_mus_hidden");
-    kontaktai.classList.add("kontaktai_hidden");
+    about.classList.add("about_hidden");
+    contact.classList.add("contact_hidden");
 }
