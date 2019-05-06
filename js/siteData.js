d65f60b8a194eeb111d38f66250f685064933fb2
naglismoc <dvd.filmas@gmai.com>
2019-05-06 16:52:26 +0300

    pridetas siteData.js failas

---
+++ js/siteData.js
@@ -0,0 +1,30 @@
+/*** site data ***** */
+
+// var myg1=[
+//     { //#naujienu mygtukas
+//     lietuviu:"naujienos",
+//     anglu:"News",
+//     rusu:"naujenkii"
+
+//     },
+   
+// ]
+
+
+
+siteData ={
+    mygtukas:
+        {
+            lietuviu:"naujienos",
+            anglu:"News",
+            rusu:"naujenkii"
+        } 
+    },
+    {
+    mygtukas:
+        {
+            lietuviu:"naujienos",
+            anglu:"News",
+            rusu:"naujenkii"
+        } 
+    }
